45d2a322a3207f253011e6022e299d6c569f00a6
zacanger <zac@zacanger.com>
2017-08-13 16:36:32 -0600

    Move some things around; remove script for example tests

--- bin/index.js
+++ bin/index.js
@@ -4,14 +4,9 @@
 // require('babel-polyfill')
 
 const { resolve } = require('path')
-const { run } = require('../lib')
+const runTests = require('../lib')
 const { name, version, description } = require('../package.json')
 const c = require('zeelib/lib/colorize').default
-
-const fail = (err) => {
-  console.error(err.message || err)
-  process.exit(err.code || 1)
-}
 
 const help = () => {
   console.log(`
@@ -32,11 +27,7 @@ const main = (argv) => {
 
   argv.forEach((a) => {
     const filename = resolve(process.cwd(), a)
-    const failed = run(filename)
-
-    if (failed) {
-      fail(new Error('Tests failed'))
-    }
+    runTests(filename)
   })
 }
 
--- examples.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for f in `ls examples`
-do
-  bin/index.js examples/$f | `npm bin`/tap-spec
-done
